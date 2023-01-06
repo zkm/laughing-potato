05bd70ac22bd8a785d50961cc8d8a6d3e933f094
Zach Schneider <me@zachschneider.com>
2023-01-06 10:21:34 -0600

    a little bit cleaner

--- php/index.php
+++ php/index.php
@@ -10,31 +10,60 @@ require_once "shipdate.php";
 // Use the getComments function
 $comments = getComments();
 
-// Display the comments for each category
-echo '<h2>Comments about candy</h2>';
-foreach ($comments['candy'] as $comment) {
-    echo $comment . '<br>';
-}
-
-echo '<h2>Comments about call me / don\'t call me</h2>';
-foreach ($comments['call me'] as $comment) {
-    echo $comment . '<br>';
-}
-
-echo '<h2>Comments about who referred me</h2>';
-foreach ($comments['referred'] as $comment) {
-    echo $comment . '<br>';
-}
-
-echo '<h2>Comments about signature requirements upon delivery</h2>';
-foreach ($comments['signature'] as $comment) {
-    echo $comment . '<br>';
-}
-
-echo '<h2>Miscellaneous comments</h2>';
-foreach ($comments['misc'] as $comment) {
-    echo $comment . '<br>';
-}
-
 // Use the updateShipDate function
 updateShipDate();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+    .wrapper {
+        width: 95%;
+        margin: 0 auto;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php 
+                        // Display the comments for each category
+                        echo '<h2>Comments about candy</h2>';
+                        foreach ($comments['candy'] as $comment) {
+                            echo $comment . '<br>';
+                        }
+
+                        echo '<h2>Comments about call me / don\'t call me</h2>';
+                        foreach ($comments['call me'] as $comment) {
+                            echo $comment . '<br>';
+                        }
+
+                        echo '<h2>Comments about who referred me</h2>';
+                        foreach ($comments['referred'] as $comment) {
+                            echo $comment . '<br>';
+                        }
+
+                        echo '<h2>Comments about signature requirements upon delivery</h2>';
+                        foreach ($comments['signature'] as $comment) {
+                            echo $comment . '<br>';
+                        }
+
+                        echo '<h2>Miscellaneous comments</h2>';
+                        foreach ($comments['misc'] as $comment) {
+                            echo $comment . '<br>';
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
